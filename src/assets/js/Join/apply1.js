a4c4f43ebc9408989bb22cfab1315de38e99c710
zxc <18675311283@163.com>
2019-02-17 17:17:52 +0800

    importOld

---
+++ src/assets/js/Join/apply1.js
@@ -0,0 +1,240 @@
+/* 
+ * @Author: anchen
+ * @Date:   2017-10-28 11:48:23
+ * @Last Modified by:   anchen
+ * @Last Modified time: 2017-11-05 23:31:12
+ */
+$(document).ready(function() {
+    /*禁止下拉*/
+    document.addEventListener('touchmove', function(e) {
+        e.preventDefault()
+    }, false);
+    /*背景图*/
+    $('.scene').parallax();
+    $('.LoadingWrap').fadeOut();
+    /*下一步滑动和menu切换*/
+    function myAddSwitch(i) {
+        $('.menu-symbol').eq(i).addClass('menu-active');
+        $('.menu-text').eq(i + 1).addClass('menu-active');
+        $('.menu-number').eq(i + 1).addClass('menu-number-active');
+        $(".banner-list").animate({
+            left: '-=6.56rem'
+        }, "fast");
+    }
+    /*上一步滑动和menu切换*/
+    function myRemoveSwitch(i) {
+        $('.menu-symbol').eq(i).removeClass('menu-active');
+        $('.menu-text').eq(i + 1).removeClass('menu-active');
+        $('.menu-number').eq(i + 1).removeClass('menu-number-active');
+        $(".banner-list").animate({
+            left: '+=6.56rem'
+        }, "fast");
+    }
+    /*按钮点击*/
+    $('.next').click(function(event) {
+        if ($('.menu-active').length == 1) {
+            myAddSwitch(0);
+            $('.previous').css({
+                display: 'block'
+            });
+            $('.next').attr('disabled', "true");
+            //判断当前为组别选择
+        } else if ($('.menu-active').length == 3) {
+            myAddSwitch(1);
+            $(this).attr({
+                'value': '完成',
+                'id': 'completed'
+            });
+            //判断当前为基本内容
+        } else if ($('.menu-active').length == 5) {
+            if (reg.test($.trim($('#Usernumber').val())) == false) {
+                phoneAlert();
+                
+               
+            } else {
+                myAjax();
+            }
+        }
+    });
+    $('.previous').click(function(event) {
+        if ($('.menu-active').length == 5) {
+            myRemoveSwitch(1);
+            $('.next').attr({
+                'value': '下一步',
+            }).removeAttr("id"); //判断当前为详细信息
+        } else if ($('.menu-active').length == 3) {
+            myRemoveSwitch(0);
+            $(this).css({
+                display: 'none'
+            }); //判断当前为基本内容
+            $('.next').removeAttr('disabled', "true");
+        }
+    });
+    /*组别介绍div的切换*/
+    $('.choice-button').click(function() {
+        var i = $(this).index();
+        //nav选中状态的切换
+        $(this).addClass('active').siblings().removeClass('active');
+        //已选中的div动态进入
+        $(".choice-introduce>div").addClass('choice-introduce-active').eq(i).siblings().removeClass('choice-introduce-active');
+        if (i !== 0) {
+            $('.next').css({
+                'background': 'rgb(51, 208, 102)',
+                'border': '1px solid rgb(51, 208, 102)'
+            }).removeAttr('disabled', "true");
+            $('.groupChoiceMsg').html('');
+        } else {
+            $('.next').css({
+                'background': 'rgba(153, 153, 153, 0.8)',
+                'border': '1px solid rgba(153, 153, 153, 0.8)'
+            }).attr('disabled', "true");
+            $('.groupChoiceMsg').html(' (现不招' + this.value + ')');
+        }
+    });
+    /*上传图片本地预览*/
+    $('#upImg').change(function() {
+        oUpImgInpt = document.getElementById('upImg').files[0];
+        var oImgsrc = window.URL.createObjectURL(oUpImgInpt);
+        $('#userimg').attr('src', oImgsrc);
+        $('#camera').remove();
+    });
+    /*报名成功弹窗*/
+    function conpeletedAlert() {
+        $('body').append('<div id="mask"></div>').append(' <div id="completedAlert"class="moveIn "><img src="img/关闭.png" alt="关闭按钮" id="close1"/><div class="completedAlertContent"><p >提交成功<br />我们期待你的加入</p><div class="completedAlertBtns"><input type="button" class="zhuyeBtn" value="确定" /></div></div></div>');
+        $('#close1').click(function(event) {
+            $(this).parent().siblings('#mask').remove();
+            $(this).parent().remove();
+        });
+        /* $('.cancleBtn').click(function(event) {
+            $(this).parent().parent().parent().siblings('#mask').remove();
+            $(this).parent().parent().parent().remove();
+        });*/
+        $('.zhuyeBtn').click(function(event) {
+            window.open('index.html', '_self');
+            $(this).parent().parent().parent().siblings('#mask').remove();
+            $(this).parent().parent().parent().remove();
+        });
+    }
+    /*重复报名弹窗*/
+    function repeatedAlert() {
+        $('body').append('<div id="mask"></div>').append(' <div class="repeatedAlert moveIn"><img src="img/关闭.png" alt="关闭按钮" id="close2"/><div class="repeatedAlertContent"><p >重复提交<br />请耐心等待面试通知</p><div class="repeatedAlertBtns"><input type="button" class="zhuyeBtn2" value="主页" /></div></div></div>');
+        $('#close2').click(function(event) {
+            $(this).parent().siblings('#mask').remove();
+            $(this).parent().remove();
+        });
+        $('.cancleBtn2').click(function(event) {
+            $(this).parent().parent().parent().siblings('#mask').remove();
+            $(this).parent().parent().parent().remove();
+        });
+        $('.zhuyeBtn2').click(function(event) {
+            window.open('index.html', '_self');
+            $(this).parent().parent().parent().siblings('#mask').remove();
+            $(this).parent().parent().parent().remove();
+        });
+    }
+    /*失败的重试弹窗*/
+    function tryAlert() {
+        $('body').append('<div id="mask"></div>').append(' <div class="repeatedAlert moveIn"><img src="img/关闭.png" alt="关闭按钮" id="close2"/><div class="repeatedAlertContent"><p >诶？有点小问题<br />请重新试试吧</p><div class="repeatedAlertBtns"><input type="button" class="repeatBtn" value="重试" /><input type="button" class="cancleBtn2" value="返回" /></div></div></div>');
+        $('#close2').click(function(event) {
+            $(this).parent().siblings('#mask').remove();
+            $(this).parent().remove();
+        });
+        $('.cancleBtn2').click(function(event) {
+            $(this).parent().parent().parent().siblings('#mask').remove();
+            $(this).parent().parent().parent().remove();
+        });
+        $('.repeatBtn').click(function(event) {
+            $(this).parent().parent().parent().siblings('#mask').remove();
+            $(this).parent().parent().parent().remove();
+            myAjax();
+        });
+    }
+    /*手机有误弹窗*/
+    function phoneAlert() {
+        $('body').append('<div id="mask"></div>').append(' <div class="repeatedAlert moveIn"><img src="img/关闭.png" alt="关闭按钮" id="close2"/><div class="repeatedAlertContent"><p >手机号码有误<br />请再仔细检查一下吧</p><div class="repeatedAlertBtns"><input type="button" class="repeatBtn" value="好的" /><input type="button" class="cancleBtn2" value="返回" /></div></div></div>');
+        $('#close2').click(function(event) {
+            $(this).parent().siblings('#mask').remove();
+            $(this).parent().remove();
+        });
+       $('.cancleBtn2').click(function(event) {
+            $(this).parent().parent().parent().siblings('#mask').remove();
+            $(this).parent().parent().parent().remove();
+        });
+      
+      $('.repeatBtn').click(function(event) {
+            $(this).parent().parent().parent().siblings('#mask').remove();
+            $(this).parent().parent().parent().remove();
+             myRemoveSwitch(1);
+                $('.next').attr({
+                'value': '下一步',
+            }).removeAttr("id"); 
+        });
+    }
+    var reg = /^1[3|4|5|7|8][0-9]{9}$/;
+    /*基本信息判空*/
+    $('.mainForm-content-baseMsg input').each(function(i) {
+        $(this).blur(function(event) {
+            if ($.trim($(this).val()) == '') {
+                $(this).parent().find("span").addClass('red');
+            } else {
+                $(this).parent().find("span").removeClass('red');
+            }
+        });
+    });
+    $('input').on('input propertychange', function() {
+        /*  if (($.trim($('#Username').val()) !== "") && ($.trim($('#Usercollege').val()) !== "") && (reg.test($.trim($('#Usernumber').val())) == true)) {
+            $('.next').removeAttr('disabled', "true");
+        } else {
+            $('.next').attr('disabled', "true");
+        }
+        if ((reg.test($.trim($('#Usernumber').val())) == false)&& ($.trim($('#Usernumber').val()) !== "")){
+           $('#Usernumber').parent().find("span").addClass('red');
+        } else {
+           $('#Usernumber').parent().find("span").removeClass('red');
+        }
+    */
+        if (($.trim($('#Username').val()) !== "") && ($.trim($('#Usercollege').val()) !== "") && ($.trim($('#Usernumber').val()) !== "")) {
+            $('.next').removeAttr('disabled', "true");
+        } else {
+            $('.next').attr('disabled', "true");
+        }
+    });
+    /*ajax*/
+    function myAjax() {
+        $('.LoadingWrap').fadeIn();
+        var UpData = new FormData(document.getElementById('FupForm')),
+            oChoiceGroup = document.getElementsByClassName('active')[0];
+        UpData.append("group", oChoiceGroup.value);
+        //Ajax
+        $.ajax({
+            url: 'https://www.ustopmoving.xin/api/application',
+            type: 'POST',
+            data: UpData,
+            contentType: false,
+            processData: false,
+            success: function(data, status) {
+                console.log(data + ':' + data.code);
+                if (data.code == 5001) {
+                    repeatedAlert();
+                    document.getElementById('FupForm').reset();
+                    /*$('#userimg').attr('src', 'img/apply/camera.png');
+                    myRemoveSwitch(1);
+                    myRemoveSwitch(0);*/
+                } else {
+                    conpeletedAlert();
+                    document.getElementById('FupForm').reset();
+                    /*$('#userimg').attr('src', 'img/apply/camera.png');*/
+                    /*myRemoveSwitch(1);
+                    myRemoveSwitch(0);
+*/
+                }
+                $('.LoadingWrap').fadeOut();
+            },
+            fail: function(err, status) {
+                tryAlert();
+                console.log(err);
+                $('.LoadingWrap').fadeOut();
+            }
+        });
+    }
+});
--- src/components/Join.vue
+++ src/components/Join.vue
@@ -1,5 +1,157 @@
 <template>
   <div id="join">
-    加入我们加入我们
+    <div id="wrap" class="mmm">
+      <div id="header">
+        <img class="LOGO" src="../assets/img/LOGO.png" alt="vtmer">
+        <router-link :to="{ name: 'home' }" class="BTN-HOME"><img src="../assets/img/BTN-HOME.png" alt="hmoe"/></router-link>
+      </div>
+      <div class="mainbody">
+        <div class="upimg">
+          <img src="../assets/img/apply/headbg.png" alt="headbg" />
+          <img src="../assets/img/apply/headqinyuan.png" height="640" width="640" alt="userimg" id="userimg" />
+          <img src="../assets/img/apply/camera.png" height="62" width="62" alt="camera" id="camera"/>
+          <input type="file" name="avatar" id='upImg' accept="image/*" form="FupForm">
+        </div>
+        <div class="mainForm">
+          <div class="mainForm-content">
+            <div class="mainForm-content-menu">
+              <div>
+                <span class="menu-number menu-number-active">①</span>
+                <span class="menu-text menu-active">选择组别</span>
+                <span class="menu-symbol">&gt;&gt;</span>
+
+                <span class="menu-number">②</span>
+                <span class="menu-text" >基本信息</span>
+                <span class="menu-symbol">&gt;&gt;</span>
+
+                <span class="menu-number">③</span>
+                <span class="menu-text">详细信息</span>
+              </div>
+            </div>
+            <div class="mainForm-content-banner">
+              <div class="banner-list" >
+                <div class="mainForm-content-groupChoice" >
+                  <div>
+                    <p>请选择组别<span class="groupChoiceMsg"> (现不招运营组)</span></p>
+                    <div class="choice-buttons">
+                      <input type="button" value="前端组" class="active choice-button" />
+                      <input type="button" value="后台组" class=" choice-button" />
+                      <input type="button" value="设计组" class=" choice-button" />
+                      <input type="button" value="运营组" class=" choice-button"  />
+                    </div>
+                    <div class="choice-introduce">
+                      <div class="choice-introduce-active"><p> 通过html，css，javascript以及衍生出来的各种库（jQuery/bootstrap/animate）、框架（AngularJS/vue.js/React.js）、解决方案来实现互联网产品的用户界面交互。<br>致力于为用户呈现更优美的页面，更流畅的交互。
+                        <br>......还有，在前台给UI和后台端茶递水。 
+                      </p></div>
+                      <div><p> PHP是世界上最好的语言，我们使用Linux系统做开发，自主搭建开发环境，致力于构建Web后台API，git，Laravel,Composer组合使用，SQL，NoSQL灵活应用，时刻掌握第一手数据。<br>(ps:给前端做苦力)
+                      </p></div>
+                      <div><p>   我们以Ps、Ai等设计软件为画笔，画出内心构想的设计蓝图，属于夜晚较为活泼的物种。<br>
+                        我们爱设计，爱生活，爱天马行空的想象。熬了无数的夜晚就是希望自己的作品能打动观看者的心。<br>
+                        爱，便义无反顾。
+                      </p></div>
+                      <div><p>
+                        我们的身影出现于产品策划，产品研发和产品维护等每一个流程，伴随产品一起发展。<br>
+                        运营组要做的事可能但不止于产品的策划、测试、优化方案、推广方案的策划、用户的沟通等等。<br>
+                        总结的来说就是：运营的执行者，产品的设计者、体验师和维护者。
+                      </p></div>
+                    </div>
+                  </div>
+
+                </div>
+                <div class="mainForm-content-baseMsg">
+                  <form name="FupForm" id='FupForm' class="baseMsgForm">
+                    <div>
+                      <p><span class="star">∗</span>姓名</p>
+                      <input type="text" id="Username" name='name'/>
+                    </div>
+                    <div>
+                      <p><span class="star">∗</span>学院专业</p>
+                      <input type="text" id="Usercollege" name='academy' />
+                    </div>
+                    <div>
+                      <p><span class="star">∗</span>联系方式</p>
+                      <input type="text" id="Usernumber" name="phone" />
+                    </div>
+                  </form>
+
+                </div>
+                <div class="mainForm-content-detailMsg">
+                  <div>
+                    <p>个人简介</p>
+                    <textarea id="signupBrief" placeholder="请在此输入简介" form='FupForm' name="intro"></textarea>
+                    <p>个人经历</p>
+                    <textarea id="signupBrief" placeholder="请在此输入经历" form="FupForm" name="exper"></textarea>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <input type="button" value="上一步"  class="previous"/>
+            <input type="button" value="下一步" class="next" disabled />
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- 背景 -->
+    <div class="BgContain">
+      <ul class="scene" id="S1" data-scalar-x="38" data-scalar-y="38">
+        <li class="layer" data-depth="0.60" id='bg1'><img src="../assets/img/bg1.png"></li>
+      </ul>
+      <ul class="scene" id="S2" data-scalar-x="38" data-scalar-y="38">
+        <li class="layer" data-depth="0.60" id='bg2'><img src="../assets/img/bg7.png"></li>
+      </ul>
+      <ul class="scene" id="S3" data-scalar-x="38" data-scalar-y="38">
+        <li class="layer" data-depth="0.40" id='bg3'><img src="../assets/img/bg3.png"></li>
+      </ul>
+      <ul class="scene" id="S4" data-scalar-x="38" data-scalar-y="38">
+        <li class="layer" data-depth="0.60" id='bg4'><img src="../assets/img/bg6.png"></li>
+      </ul>
+      <ul class="scene" id="S5" data-scalar-x="38" data-scalar-y="38">
+        <li class="layer" data-depth="0.80" id='bg5'><img src="../assets/img/bg4.png"></li>
+      </ul>
+      <ul class="scene" id="S6" data-scalar-x="38" data-scalar-y="38">
+        <li class="layer" data-depth="1.00" id='bg6'><img src="../assets/img/bg9.png"></li>
+      </ul>
+      <ul class="scene" id="S7" data-scalar-x="28" data-scalar-y="28">
+        <li class="layer" data-depth="1.00" id='bg7'><img src="../assets/img/bg11.png"></li>
+      </ul>
+      <ul class="scene" id="S8" data-scalar-x="28" data-scalar-y="28">
+        <li class="layer" data-depth="0.60" id='bg4'><img src="../assets/img/bg10.png"></li>
+      </ul>
+      <ul class="scene" id="S9" data-scalar-x="28" data-scalar-y="28">
+        <li class="layer" data-depth="0.80" id='bg5'><img src="../assets/img/bg8.png"></li>
+      </ul>
+      <ul class="scene" id="S10" data-scalar-x="28" data-scalar-y="28">
+        <li class="layer" data-depth="1.00" id='bg6'><img src="../assets/img/bg2.png"></li>
+      </ul>
+      <ul class="scene" id="S11" data-scalar-x="28" data-scalar-y="28">
+        <li class="layer" data-depth="1.00" id='bg6'><img src="../assets/img/bg12.png"></li>
+      </ul>
+      <ul class="scene" id="S12" data-scalar-x="28" data-scalar-y="28">
+        <li class="layer" data-depth="1.00" id='bg6'><img src="../assets/img/bg5.png"></li>
+      </ul>
+    </div>
+    <!-- Loading提交 -->
+    <div class="LoadingWrap">
+      <div class="loading">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
   </div>
 </template>
+<style scoped>
+  @import '../assets/css/Join/apply.css';
+  @import '../assets/css/Join/bg.css';
+  @import '../assets/css/Join/swiper-3.4.2.min.css';
+  /*@import '../assets/img/vtmerLogin.ico';*/
+
+</style>
+<script>
+  import '../assets/js/jq.min';
+  import '../assets/js/jquery.parallax.min';
+  import '../assets/js/Join/apply1';
+
+</script>
